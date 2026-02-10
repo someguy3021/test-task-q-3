933010247dede5b88abb75ea29b34125af6bb216
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2026-02-10 11:41:27 +0500

    Update ErrorNotFound.vue

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,27 +1,10 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen text-center q-pa-md flex flex-center">
     <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
-
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
-
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
+      <div style="font-size: 30vh">404</div>
+      <div class="text-h2" style="opacity:.4">{{ $t('pages.ErrorNotFound.title') }}</div>
+      <div class="text-h5 q-mt-md">{{ $t('pages.ErrorNotFound.message') }}</div>
+      <q-btn class="q-mt-xl" color="primary" unelevated to="/" :label="$t('pages.ErrorNotFound.goHome')" no-caps />
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-//
-</script>
